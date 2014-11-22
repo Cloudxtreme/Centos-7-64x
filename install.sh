6384bd7fbe81109ca2677ec03e4d1867b006fb11
Huy Tran <vbaothu@gmail.com>
2014-11-23 02:46:20 +0700

    Update
    Update

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-#GOTO TMP FOLDER
-cd /tmp/
-
+#CLEAN BEFORE INSTALL
+clear
+yum clean all
+yum update
 # GET INFO CPU
-cpuname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
-cpucores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-cpufreq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
-svram=$( free -m | awk 'NR==2 {print $2}' )
-svhdd=$( df -h | awk 'NR==2 {print $2}' )
-svswap=$( free -m | awk 'NR==4 {print $2}' )
+number_cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
 
 #STOP, REMOVE, REMOVE SOMETHING
 
@@ -42,12 +38,21 @@ rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0
 
 yum install nginx
 systemctl enable nginx.service
+systemctl restart nginx.service
 
 #INSTALL MARIA DATABASE
 yum install mariadb-server mariadb
 systemctl start mariadb
 mysql_secure_installation
+cp -fr /tmp/Centos-7-64x/sources/my.cnf /etc/
+
+systemctl enable mariadb.service
+systemctl restart mariadb.service
 
 #INSTALL PHP
 yum install php php-mysql php-fpm
+
+cp -fr /tmp/Centos-7-64x/sources/php.ini /etc/
 sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php.ini
+cp -fr /tmp/Centos-7-64x/sources/nginx.conf /etc/nginx/
+sed -i 's/number_cores/$number_cores/g' /etc/nginx/nginx.conf
