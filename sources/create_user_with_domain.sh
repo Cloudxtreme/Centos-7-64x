4eafdadc8cc7adb1bb3290ed4f85fe7fc26875b3
Huy Tran <vbaothu@gmail.com>
2014-11-23 20:01:47 +0700

    Update Home Nginx

--- sources/create_user_with_domain.sh
+++ sources/create_user_with_domain.sh
@@ -21,8 +21,8 @@ echo $PASS | passwd --stdin $USERNAME
 CONFIG=$NGINX_SITE_CONF/$DOMAIN.conf
 cp $SOURCES_DIR/nginx.vhost.conf $CONFIG
 $SED -i "s/@@HOSTNAME@@/$DOMAIN/g" $CONFIG
-$SED -i "s#@@PATH@@#\/home\/"$USERNAME/public_html"#g" $CONFIG
-$SED -i "s/@@LOG_PATH@@/\/home\/$USERNAME\/_logs/g" $CONFIG
+$SED -i "s#@@PATH@@#\/home\/nginx\/"$USERNAME/public_html"#g" $CONFIG
+$SED -i "s/@@LOG_PATH@@/\/home\/nginx\/$USERNAME\/_logs/g" $CONFIG
 $SED -i "s#@@SOCKET@@#/var/run/php-fpm/"$USERNAME"_fpm.sock#g" $CONFIG
 echo "How many FPM servers would you like by default:"
 read FPM_SERVERS
@@ -36,7 +36,7 @@ FPMCONF="$PHP_POOL_DIR/$DOMAIN.pool.conf"
 cp $SOURCES_DIR/pool.conf $FPMCONF
 
 $SED -i "s/@@USER@@/$USERNAME/g" $FPMCONF
-$SED -i "s/@@HOME_DIR@@/\/home\/$USERNAME/g" $FPMCONF
+$SED -i "s/@@HOME_DIR@@/\/home\/nginx\/$USERNAME/g" $FPMCONF
 $SED -i "s/@@START_SERVERS@@/$FPM_SERVERS/g" $FPMCONF
 $SED -i "s/@@MIN_SERVERS@@/$MIN_SERVERS/g" $FPMCONF
 $SED -i "s/@@MAX_SERVERS@@/$MAX_SERVERS/g" $FPMCONF
@@ -45,20 +45,22 @@ $SED -i "s/@@MAX_CHILDS@@/$MAX_CHILDS/g" $FPMCONF
 
 #ADD NGINX TO GROUP, SO IT CAN PASS ROUTES
 usermod -aG $USERNAME nginx
-chmod g+rx /home/$HOME_DIR
+chmod g+rx /home/nginx/$HOME_DIR
 chmod 600 $CONFIG
 
 #CREATE TEST PHPINFO FILE
+touch /home/nginx/$HOME_DIR/public_html/index.php
 cat > "/home/$HOME_DIR/public_html/index.php" <<END
 <?php phpinfo(); ?>
 END
 
 # set file perms and create required dirs!
-mkdir -p /home/$HOME_DIR/public_html
-mkdir /home/$HOME_DIR/_logs
-mkdir /home/$HOME_DIR/_sessions
-chmod 750 /home/$HOME_DIR -R
-chmod 700 /home/$HOME_DIR/_sessions
-chmod 770 /home/$HOME_DIR/_logs
-chmod 750 /home/$HOME_DIR/public_html
-chown $USERNAME:$USERNAME /home/$HOME_DIR/ -R
+mkdir -p /home/nginx/$HOME_DIR/public_html
+mkdir /home/nginx/$HOME_DIR/_logs
+mkdir /home/nginx/$HOME_DIR/_sessions
+chmod 750 /home/nginx/$HOME_DIR -R
+chmod 700 /home/nginx/$HOME_DIR/_sessions
+chmod 770 /home/nginx/$HOME_DIR/_logs
+chmod 750 /home/nginx/$HOME_DIR/public_html
+chown -R $USERNAME:$USERNAME /home/nginx/$HOME_DIR/
+chown -R nginx:nginx /home/nginx
