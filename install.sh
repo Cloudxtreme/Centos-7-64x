803d182dc5cf2caf239cb10277c9046fbe537fba
Huy Tran <vbaothu@gmail.com>
2014-11-23 07:42:54 +0700

    Update MariaDB
    Fix conflict with old version (5.5)

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ yum --enablerepo=remi,remi-php56 install -y nginx php-fpm php-common
 systemctl enable nginx.service
 
 #INSTALL MARIA DATABASE
-rpm -ev mariadb*
+yum -y remove mariadb-libs-5.5.40-1.el7_0.x86_64
 
 cp -fr /tmp/Centos-7-64x/sources/MariaDB.repo /etc/yum.repos.d/
 yum -y update
